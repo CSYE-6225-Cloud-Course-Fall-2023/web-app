5ba41ba146fdb288c74f4a8de51b6d9d31e90dee
dalavaiyuvaraj <dalavai.y@northeastern.edu>
2023-10-25 08:01:59 -0400

    removed user creation in script

--- install.sh
+++ install.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
 
-# Set the root password for MariaDB
-ROOT_PASSWORD="Yuvarajmy@143"
-
-# Database name to create
-DB_NAME="Cloud_db"
-
-# New user information
-NEW_USER="admin"
-NEW_USER_PASSWORD="Admin@143"
-
 # Specify the path to the zip file
 ZIP_FILE="/tmp/Yuvaraj_dalavai_002789615_03.zip"
 
@@ -35,16 +25,6 @@ sudo apt-get install -y unzip
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
 
-# Set the root password for MariaDB
-sudo mysqladmin -u root password "$ROOT_PASSWORD"
-
-# Create the "Cloud_db" database
-sudo mysql -u root -p"$ROOT_PASSWORD" -e "CREATE DATABASE $DB_NAME;"
-
-# Create a new MariaDB user with administrative privileges
-sudo mysql -u root -p"$ROOT_PASSWORD" -e "CREATE USER '$NEW_USER'@'localhost' IDENTIFIED BY '$NEW_USER_PASSWORD';"
-sudo mysql -u root -p"$ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON *.* TO '$NEW_USER'@'localhost' WITH GRANT OPTION;"
-sudo mysql -u root -p"$ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
 
 # Unzip the file to the destination directory
 sudo unzip "$ZIP_FILE" -d "$DEST_DIR"
